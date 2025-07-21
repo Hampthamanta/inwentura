e7fb9425d09c296b5ddac82b1f5ec5427d4a77a3
Hampthamanta <mushroomepic@gmail.com>
2025-07-21 16:17:08 +0200

    v6 DB + opt

--- app/src/main/java/com/example/project_v1/MainActivity.kt
+++ app/src/main/java/com/example/project_v1/MainActivity.kt
@@ -51,6 +51,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
